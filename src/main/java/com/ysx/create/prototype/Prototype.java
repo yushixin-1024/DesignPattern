4faf50d5e18e3ea470b914a1bb649b85dfb1cc5c
Urban-ysx <1605006939@qq.com>
2021-08-31 12:32:48 +0800

    原型模式类图

--- src/main/java/com/ysx/create/prototype/Prototype.java
+++ src/main/java/com/ysx/create/prototype/Prototype.java
@@ -97,6 +97,30 @@ public class Prototype implements Serializable, Cloneable {
         }
     }
 
+    public static void main(String[] args) throws CloneNotSupportedException {
+        // clone
+        Prototype prototype = new Prototype(10, "Prototype");
+        Prototype clone = (Prototype) prototype.clone();
+        System.out.println("prototype:" + prototype);
+        System.out.println("prototype-hashCode:" + prototype.hashCode());
+        System.out.println("prototype-getInner-hashCode:" + prototype.getInner().hashCode());
+        System.out.println("clone:" + clone.toString());
+        System.out.println("clone-hashCode:" + clone.hashCode());
+        System.out.println("clone-getInner-hashCode:" + clone.getInner().hashCode());
+
+        // 序列化
+        Prototype prototype1 = new Prototype(10, "Prototype");
+        Prototype serialize = (Prototype) prototype1.serialize();
+        System.out.println("prototype1:" + prototype1);
+        System.out.println("prototype1-hashCode:" + prototype1.hashCode());
+        System.out.println("prototype1-getInner-hashCode:" + prototype1.getInner().hashCode());
+        System.out.println("serialize:" + serialize.toString());
+        System.out.println("serialize-hashCode:" + serialize.hashCode());
+        System.out.println("serialize-getInner-hashCode:" + serialize.getInner().hashCode());
+
+
+    }
+
     /**
      * 内部类
      */
